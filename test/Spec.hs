9941e666771635a1abe6af2655936853a9c5f748
Bryan Garza <brygarza@gmail.com>
2015-10-31 19:31:35 -0700

    Add 2 pawn tests

---
+++ test/Spec.hs
@@ -0,0 +1,30 @@
+{-# LANGUAGE BinaryLiterals #-}
+
+module Main where
+
+import Cheese
+
+import Data.Word (Word64)
+import Test.Hspec
+
+absolute :: Int -> Int
+absolute = undefined
+
+main :: IO ()
+main = hspec $ do
+  describe "pawns" $ do
+    it "returns moves for starting position" $
+      whitePawnMoves initialBoard `shouldBe` 0b0000000000000000000000000000000011111111111111110000000000000000
+
+    it "returns moves for staggered position" $ do
+      -- ⬜ ⬜ ⬜ ⬜ ⬜ ⬜ ⬜ ⬜    ⬜ ⬜ ⬜ ⬜ ⬜ ⬛ ⬜ ⬜
+      -- ⬜ ⬜ ⬜ ⬜ ⬜ ♙ ⬜ ⬜    ⬜ ⬛ ⬜ ⬜ ⬜ ⬜ ⬜ ⬜
+      -- ⬜ ♙ ⬜ ⬜ ⬜ ⬜ ⬜ ⬜    ⬜ ⬜ ⬛ ⬜ ⬜ ⬜ ⬜ ⬜
+      -- ⬜ ⬜ ♙ ⬜ ⬜ ⬜ ⬜ ⬜    ⬜ ⬜ ⬜ ⬜ ⬜ ⬜ ⬜ ⬜
+      -- ⬜ ⬜ ⬜ ⬜ ⬜ ⬜ ⬜ ⬜    ⬛ ⬜ ⬜ ⬛ ⬜ ⬛ ⬜ ⬛
+      -- ♙ ⬜ ⬜ ⬜ ⬜ ♙ ⬜ ♙    ⬜ ⬜ ⬜ ⬛ ⬜ ⬜ ⬜ ⬜
+      -- ⬜ ⬜ ⬜ ♙ ⬜ ⬜ ⬜ ⬜    ⬜ ⬜ ⬜ ⬜ ⬜ ⬜ ⬜ ⬜
+      -- ⬜ ⬜ ⬜ ⬜ ⬜ ⬜ ⬜ ⬜    ⬜ ⬜ ⬜ ⬜ ⬜ ⬜ ⬜ ⬜
+      let board = emptyBoard { whitePawns = 0b0000000000100000000000100000010000000000101000010000100000000000 }
+      let layer = 0b0010000000000010000001000000000010101001000010000000000000000000
+      whitePawnMoves board `shouldBe` layer
