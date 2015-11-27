a18f70769b9b9673659920b2ca643b24d6990eab
Bryan Garza <brygarza@gmail.com>
2015-11-26 16:17:56 -0800

    Add another pawn test

--- test/Spec.hs
+++ test/Spec.hs
@@ -7,24 +7,52 @@ import Cheese
 import Data.Word (Word64)
 import Test.Hspec
 
-absolute :: Int -> Int
-absolute = undefined
-
 main :: IO ()
 main = hspec $ do
   describe "pawns" $ do
-    it "returns moves for starting position" $
-      whitePawnMoves initialBoard `shouldBe` 0b0000000000000000000000000000000011111111111111110000000000000000
+    describe "returns moves for" $ do
+       it "starting position" $
+         whitePawnMoves initialBoard `shouldBe` 0b0000000000000000000000000000000011111111111111110000000000000000
 
-    it "returns moves for staggered position" $
-      -- ⬜ ⬜ ⬜ ⬜ ⬜ ⬜ ⬜ ⬜    ⬜ ⬜ ⬜ ⬜ ⬜ ⬛ ⬜ ⬜
-      -- ⬜ ⬜ ⬜ ⬜ ⬜ ♙ ⬜ ⬜    ⬜ ⬛ ⬜ ⬜ ⬜ ⬜ ⬜ ⬜
-      -- ⬜ ♙ ⬜ ⬜ ⬜ ⬜ ⬜ ⬜    ⬜ ⬜ ⬛ ⬜ ⬜ ⬜ ⬜ ⬜
-      -- ⬜ ⬜ ♙ ⬜ ⬜ ⬜ ⬜ ⬜    ⬜ ⬜ ⬜ ⬜ ⬜ ⬜ ⬜ ⬜
-      -- ⬜ ⬜ ⬜ ⬜ ⬜ ⬜ ⬜ ⬜    ⬛ ⬜ ⬜ ⬛ ⬜ ⬛ ⬜ ⬛
-      -- ♙ ⬜ ⬜ ⬜ ⬜ ♙ ⬜ ♙    ⬜ ⬜ ⬜ ⬛ ⬜ ⬜ ⬜ ⬜
-      -- ⬜ ⬜ ⬜ ♙ ⬜ ⬜ ⬜ ⬜    ⬜ ⬜ ⬜ ⬜ ⬜ ⬜ ⬜ ⬜
-      -- ⬜ ⬜ ⬜ ⬜ ⬜ ⬜ ⬜ ⬜    ⬜ ⬜ ⬜ ⬜ ⬜ ⬜ ⬜ ⬜
-      whitePawnMoves board `shouldBe` layer
-        where board = emptyBoard { whitePawns = 0b0000000000100000000000100000010000000000101000010000100000000000 }
-              layer = 0b0010000000000010000001000000000010101001000010000000000000000000
+       it "staggered position" $ do
+         -- ⬜ ⬜ ⬜ ⬜ ⬜ ⬜ ⬜ ⬜    ⬜ ⬜ ⬜ ⬜ ⬜ ⬛ ⬜ ⬜
+         -- ⬜ ⬜ ⬜ ⬜ ⬜ ♙ ⬜ ⬜    ⬜ ⬛ ⬜ ⬜ ⬜ ⬜ ⬜ ⬜
+         -- ⬜ ♙ ⬜ ⬜ ⬜ ⬜ ⬜ ⬜    ⬜ ⬜ ⬛ ⬜ ⬜ ⬜ ⬜ ⬜
+         -- ⬜ ⬜ ♙ ⬜ ⬜ ⬜ ⬜ ⬜    ⬜ ⬜ ⬜ ⬜ ⬜ ⬜ ⬜ ⬜
+         -- ⬜ ⬜ ⬜ ⬜ ⬜ ⬜ ⬜ ⬜    ⬛ ⬜ ⬜ ⬛ ⬜ ⬛ ⬜ ⬛
+         -- ♙ ⬜ ⬜ ⬜ ⬜ ♙ ⬜ ♙    ⬜ ⬜ ⬜ ⬛ ⬜ ⬜ ⬜ ⬜
+         -- ⬜ ⬜ ⬜ ♙ ⬜ ⬜ ⬜ ⬜    ⬜ ⬜ ⬜ ⬜ ⬜ ⬜ ⬜ ⬜
+         -- ⬜ ⬜ ⬜ ⬜ ⬜ ⬜ ⬜ ⬜    ⬜ ⬜ ⬜ ⬜ ⬜ ⬜ ⬜ ⬜
+         let board = emptyBoard { whitePawns = 0b0000000000100000000000100000010000000000101000010000100000000000 }
+             layer = 0b0010000000000010000001000000000010101001000010000000000000000000
+         whitePawnMoves board `shouldBe` layer
+
+       it "capture or move up (centered, isolated)" $ do
+         -- ⬜ ⬜ ⬜ ⬜ ⬜ ⬜ ⬜ ⬜   ⬜ ⬜ ⬜ ⬜ ⬜ ⬜ ⬜ ⬜
+         -- ⬜ ⬜ ⬜ ⬜ ⬜ ⬜ ⬜ ⬜   ⬜ ⬜ ⬜ ⬜ ⬜ ⬜ ⬜ ⬜
+         -- ⬜ ⬜ ⬜ ⬜ ⬜ ⬜ ⬜ ⬜   ⬜ ⬜ ⬜ ⬜ ⬜ ⬜ ⬜ ⬜
+         -- ⬜ ⬜ ⬜ ♟ ⬜ ⬜ ⬜ ⬜   ⬜ ⬜ ⬜ ♙ ♙ ⬜ ⬜ ⬜
+         -- ⬜ ⬜ ⬜ ⬜ ♙ ⬜ ⬜ ⬜   ⬜ ⬜ ⬜ ⬜ ⬜ ⬜ ⬜ ⬜
+         -- ⬜ ⬜ ⬜ ⬜ ⬜ ⬜ ⬜ ⬜   ⬜ ⬜ ⬜ ⬜ ⬜ ⬜ ⬜ ⬜
+         -- ⬜ ⬜ ⬜ ⬜ ⬜ ⬜ ⬜ ⬜   ⬜ ⬜ ⬜ ⬜ ⬜ ⬜ ⬜ ⬜
+         -- ⬜ ⬜ ⬜ ⬜ ⬜ ⬜ ⬜ ⬜   ⬜ ⬜ ⬜ ⬜ ⬜ ⬜ ⬜ ⬜
+         let board = emptyBoard { whitePawns = 0b0000000000000000000000000000000000010000000000000000000000000000,
+                                  blackPawns = 0b0000000000000000000000000000100000000000000000000000000000000000
+                                }
+             layer = 0b0000000000000000000000000001100000000000000000000000000000000000
+         whitePawnMoves board `shouldBe` layer
+
+       it "possible overflow" $ do
+         -- ⬜ ⬜ ⬜ ⬜ ⬜ ⬜ ⬜ ⬜   ⬜ ⬜ ⬜ ⬜ ⬜ ⬜ ⬜ ⬜
+         -- ⬜ ⬜ ⬜ ⬜ ⬜ ⬜ ⬜ ⬜   ⬜ ⬜ ⬜ ⬜ ⬜ ⬜ ⬜ ⬜
+         -- ⬜ ⬜ ⬜ ⬜ ⬜ ⬜ ⬜ ⬜   ⬜ ⬜ ⬜ ⬜ ⬜ ⬜ ⬜ ⬜
+         -- ⬜ ⬜ ⬜ ⬜ ⬜ ⬜ ⬜ ⬜   ⬜ ⬜ ⬜ ⬜ ⬜ ⬜ ⬜ ⬜
+         -- ⬜ ⬜ ⬜ ⬜ ⬜ ⬜ ⬜ ♟   ♙ ⬜ ⬜ ⬜ ⬜ ⬜ ⬜ ⬜
+         -- ♙ ⬜ ⬜ ⬜ ⬜ ⬜ ⬜ ⬜   ⬜ ⬜ ⬜ ⬜ ⬜ ⬜ ⬜ ⬜
+         -- ⬜ ⬜ ⬜ ⬜ ⬜ ⬜ ⬜ ⬜   ⬜ ⬜ ⬜ ⬜ ⬜ ⬜ ⬜ ⬜
+         -- ⬜ ⬜ ⬜ ⬜ ⬜ ⬜ ⬜ ⬜   ⬜ ⬜ ⬜ ⬜ ⬜ ⬜ ⬜ ⬜
+         let board = emptyBoard { whitePawns   = 0b0000000000000000000000000000000000000000000000010000000000000000,
+                                  blackPawns   = 0b0000000000000000000000000000000010000000000000000000000000000000
+                                }
+             layer = 0b0000000000000000000000000000000000000001000000000000000000000000
+         whitePawnMoves board `shouldBe` layer
